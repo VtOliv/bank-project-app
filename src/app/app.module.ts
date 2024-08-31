52c8141ddf86768d57a46621cd47e97568e3f3f4
Vitor <vitorturmac@gmail.com>
2024-08-31 00:15:54 -0300

    background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,8 +34,8 @@ import { InputOtpModule } from 'primeng/inputotp';
     ExtratoComponent
   ],
   imports: [
-    BrowserModule,
     BrowserAnimationsModule,
+    BrowserModule,
     HttpClientModule,
     FormsModule,
     AppRoutingModule,
